7b8e4a7d7e524187bc4b140c86366a1f367a9932
Haroon <haroon.ali.1104@gmail.com>
2023-06-08 22:21:03 +0100

    store token locally and navigate to login page on successful login

--- client/src/components/RouteLogin.js
+++ client/src/components/RouteLogin.js
@@ -1,95 +1,100 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Notification from "./Notification";
 
 const RouteLogin = () => {
-  const [notification, setNotification] = useState({
-    message: "",
-    display: "none",
-    bgColor: ""
-  });
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-  const submit = async (e) => {
-    e.preventDefault();
+    const [notification, setNotification] = useState({
+        message: "",
+        display: "none",
+        bgColor: ""
+    });
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    try {
-      const res = await fetch("http://localhost:3001/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password })
-      });
+    const submit = async (e) => {
+        e.preventDefault();
 
-      const data = await res.json();
+        try {
+            const res = await fetch("http://localhost:3001/api/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password })
+            });
 
-      if (res.status === 200) {
-        console.log(data.token);
-      } else {
-        setNotification({
-          message: data.message,
-          display: "block",
-          bgColor: "#E2412E"
-        });
-      }
-    } catch (error) {
-      setNotification({
-        message: error.message,
-        display: "block",
-        bgColor: "#E2412E"
-      });
-    } finally {
-      setTimeout(() => {
-        setNotification({
-          message: "",
-          display: "none",
-          bgColor: "#E2412E"
-        });
-      }, 3000);
+            const data = await res.json();
+
+            if (res.status === 200) {
+                localStorage.setItem("token", data.token);
+
+                navigate("/customers");
+            } else {
+                setNotification({
+                    message: data.message,
+                    display: "block",
+                    bgColor: "#E2412E"
+                });
+            }
+        } catch (error) {
+            setNotification({
+                message: error.message,
+                display: "block",
+                bgColor: "#E2412E"
+            });
+        } finally {
+            setTimeout(() => {
+                setNotification({
+                    message: "",
+                    display: "none",
+                    bgColor: "#E2412E"
+                });
+            }, 3000);
+        }
     }
-  }
 
-  return (
-    <div className="cont">
-      <form onSubmit={submit}>
-        <div className="form-input-container">
-          <label>
-            Email
-            <br />
-            <input
-              id="email"
-              type="text"
-              value={email}
-              onChange={(e) => { setEmail(e.target.value); }}
-              minLength="3"
-              maxLength="256"
-              required />
-          </label>
+    return (
+        <div className="cont">
+            <form onSubmit={submit}>
+                <div className="form-input-container">
+                    <label>
+                        Email
+                        <br />
+                        <input
+                            id="email"
+                            type="text"
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value); }}
+                            minLength="3"
+                            maxLength="256"
+                            required />
+                    </label>
+                </div>
+                <div className="form-input-container">
+                    <label>
+                        Password
+                        <br />
+                        <input
+                            id="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value); }}
+                            minLength="3"
+                            maxLength="15"
+                            required />
+                    </label>
+                </div>
+                <div className="form-submit-container">
+                    <button className="btn btn-add" id="btn-add" type="submit">Login</button>
+                </div>
+            </form>
+            <Notification
+                message={notification.message}
+                display={notification.display}
+                bgColor={notification.bgColor}
+            />
         </div>
-        <div className="form-input-container">
-          <label>
-            Password
-            <br />
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => { setPassword(e.target.value); }}
-              minLength="3"
-              maxLength="15"
-              required />
-          </label>
-        </div>
-        <div className="form-submit-container">
-          <button className="btn btn-add" id="btn-add" type="submit">Login</button>
-        </div>
-      </form>
-      <Notification
-        message={notification.message}
-        display={notification.display}
-        bgColor={notification.bgColor}
-      />
-    </div>
-  );
+    );
 }
 
 export default RouteLogin;
