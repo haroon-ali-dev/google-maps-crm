42ef1ea27b5c4cbcdf0940c85847af0e69788f20
Haroon <haroon.ali.1104@gmail.com>
2023-06-08 15:09:49 +0100

    create register route component

---
+++ client/src/components/RouteRegister.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const RouteRegister = () => {
+  const [email, setEmail] = useState([]);
+
+  return (
+    <div>
+      <h1 className="heading">Simple Customer Relationship Manager</h1>
+      <div className="cont">
+        
+      </div>
+    </div>
+  );
+}
+
+export default RouteRegister;
