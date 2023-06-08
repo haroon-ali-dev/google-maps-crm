b1e8fe6679c0408d855d6b77362b4ba71faf902b
Haroon <haroon.ali.1104@gmail.com>
2023-06-08 15:38:20 +0100

    create user model

---
+++ api/model/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 256
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 10
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validate(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(3).max(256).required(),
+    password: Joi.string().min(3).max(10).required()
+  });
+
+  return schema.validate(user);
+}
+
+module.exports.User = User;
+module.exports.validate = validate;
