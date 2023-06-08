3e50dbb8ce786cb2d2beb867e2a9d44ebdc353b3
Haroon <haroon.ali.1104@gmail.com>
2023-06-08 22:38:19 +0100

    create navbar with buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
+  const token = localStorage.getItem("token");
+
   return (
-    <div>
+    <header>
+      <nav className="navbar">
+        {!token &&
+          <>
+            <Link>Register</Link>
+            <Link>Login</Link>
+          </>
+        }
+        {token && <button>Logout</button>}
+      </nav>
       <h1 className="heading">Simple Customer Relationship Manager</h1>
-    </div>
+    </header>
   );
 }
 
