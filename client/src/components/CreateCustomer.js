a95bc854937926ec838cf20da1f8a68ce717eca6
Haroon <haroon.ali.1104@gmail.com>
2023-06-09 19:47:17 +0100

    add animations to customer create form

--- client/src/components/CreateCustomer.js
+++ client/src/components/CreateCustomer.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import jwt from "jwt-decode";
+import { motion, AnimatePresence } from "framer-motion";
+import Notification from "./Notification";
 
 const CreateCustomer = ({ createCustomer }) => {
+  const [notification, setNotification] = useState({
+    message: "",
+    display: false,
+    bgColor: ""
+  });
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
@@ -16,21 +23,37 @@ const CreateCustomer = ({ createCustomer }) => {
     }
 
     try {
-      const res = await fetch("http://localhost:3001/api/customers", {
+      const res = await fetch("http://localhost:30012/api/customers", {
         method: "POST",
         headers: { "Content-Type": "application/json", "x-auth-token": token },
         body: JSON.stringify({ userId: uId, name, email })
       });
 
       const data = await res.json();
-      if (data.message === "success") {
+
+      if (res.status === 200) {
         createCustomer(data.customer);
       } else {
-        alert(data.message);
+        setNotification({
+          message: data.message,
+          display: true,
+          bgColor: "#E2412E"
+        });
       }
-
     } catch (error) {
-      alert(error.message);
+      setNotification({
+        message: error.message,
+        display: true,
+        bgColor: "#E2412E"
+      });
+    } finally {
+      setTimeout(() => {
+        setNotification({
+          message: "",
+          display: false,
+          bgColor: "#E2412E"
+        });
+      }, 3000);
     }
   }
 
@@ -55,6 +78,21 @@ const CreateCustomer = ({ createCustomer }) => {
           <button className="btn btn-add" id="btn-add" type="submit">Add</button>
         </div>
       </form>
+      <AnimatePresence>
+        {notification.display && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
+            exit={{ opacity: 0 }}
+          >
+            <Notification
+              message={notification.message}
+              bgColor={notification.bgColor}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
