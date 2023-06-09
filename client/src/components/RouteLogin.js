b967d52b296e830cd34122441fcb4fcd35360f7c
Haroon <haroon.ali.1104@gmail.com>
2023-06-09 18:19:42 +0100

    animate login page

--- client/src/components/RouteLogin.js
+++ client/src/components/RouteLogin.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion"
 import Notification from "./Notification";
 
 const RouteLogin = () => {
@@ -7,7 +8,7 @@ const RouteLogin = () => {
 
     const [notification, setNotification] = useState({
         message: "",
-        display: "none",
+        display: false,
         bgColor: ""
     });
     const [email, setEmail] = useState("");
@@ -32,21 +33,21 @@ const RouteLogin = () => {
             } else {
                 setNotification({
                     message: data.message,
-                    display: "block",
+                    display: true,
                     bgColor: "#E2412E"
                 });
             }
         } catch (error) {
             setNotification({
                 message: error.message,
-                display: "block",
+                display: true,
                 bgColor: "#E2412E"
             });
         } finally {
             setTimeout(() => {
                 setNotification({
                     message: "",
-                    display: "none",
+                    display: false,
                     bgColor: "#E2412E"
                 });
             }, 3000);
@@ -54,46 +55,62 @@ const RouteLogin = () => {
     }
 
     return (
-        <div className="cont">
-            <form onSubmit={submit}>
-                <div className="form-input-container">
-                    <label>
-                        Email
-                        <br />
-                        <input
-                            id="email"
-                            type="text"
-                            value={email}
-                            onChange={(e) => { setEmail(e.target.value); }}
-                            minLength="3"
-                            maxLength="256"
-                            required />
-                    </label>
-                </div>
-                <div className="form-input-container">
-                    <label>
-                        Password
-                        <br />
-                        <input
-                            id="password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => { setPassword(e.target.value); }}
-                            minLength="3"
-                            maxLength="15"
-                            required />
-                    </label>
-                </div>
-                <div className="form-submit-container">
-                    <button className="btn btn-add" id="btn-add" type="submit">Login</button>
-                </div>
-            </form>
-            <Notification
-                message={notification.message}
-                display={notification.display}
-                bgColor={notification.bgColor}
-            />
-        </div>
+        <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
+        >
+            <div className="cont">
+                <form onSubmit={submit}>
+                    <div className="form-input-container">
+                        <label>
+                            Email
+                            <br />
+                            <input
+                                id="email"
+                                type="text"
+                                value={email}
+                                onChange={(e) => { setEmail(e.target.value); }}
+                                minLength="3"
+                                maxLength="256"
+                                required />
+                        </label>
+                    </div>
+                    <div className="form-input-container">
+                        <label>
+                            Password
+                            <br />
+                            <input
+                                id="password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => { setPassword(e.target.value); }}
+                                minLength="3"
+                                maxLength="15"
+                                required />
+                        </label>
+                    </div>
+                    <div className="form-submit-container">
+                        <button className="btn btn-add" id="btn-add" type="submit">Login</button>
+                    </div>
+                </form>
+                <AnimatePresence>
+                    {notification.display && (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ ease: "easeOut", duration: 1.5 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <Notification
+                                message={notification.message}
+                                bgColor={notification.bgColor}
+                            />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
+        </motion.div>
     );
 }
 
