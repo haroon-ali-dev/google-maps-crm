63d2931e57bcfc7e4c36af78808c402a039d09a2
Haroon <haroon.ali.1104@gmail.com>
2023-08-07 15:47:08 +0100

    add e2e test for email validation on register page

--- client/cypress/e2e/register-page.cy.js
+++ client/cypress/e2e/register-page.cy.js
@@ -1,7 +1,32 @@
-describe('register page', () => {
-  it('form is validated', () => {
+describe('Form validation', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+  });
 
-    
+  it('Fields cannot be empty', () => {
+    cy.get('#btn-add').click();
+    cy.contains('Email is a required field').should('exist');
+  })
+
+  it('Email must be at least 3 characters', () => {
+    cy.get('#email').type('h');
+    cy.get('#btn-add').click();
+    cy.contains('Email must be a valid email').should('exist');
+  })
+
+  it('Email must be a valid email', () => {
+    cy.get('#email').type('bobemail');
+    cy.get('#btn-add').click();
+    cy.contains('Email must be a valid email').should('exist');
+  })
+
+  it('Email cannot be longer than 256 characters', () => {
+    let longEmail = 'bobemail';
+    longEmail += Cypress._.repeat('l', 250);
+    longEmail += '@gmail.com';
+
+    cy.get('#email').type(longEmail);
+    cy.get('#btn-add').click();
+    cy.contains('Email must be at most 256 characters').should('exist');
   })
 })
