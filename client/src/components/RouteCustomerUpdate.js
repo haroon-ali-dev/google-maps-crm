0165002438f122061b8b9902f2585e8e49f9932d
Haroon <haroon.ali.1104@gmail.com>
2023-08-14 15:45:22 +0100

    implement react-hook-form validation on customer update page

--- client/src/components/RouteCustomerUpdate.js
+++ client/src/components/RouteCustomerUpdate.js
@@ -3,7 +3,16 @@ import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import jwt from "jwt-decode";
 import { motion, AnimatePresence } from "framer-motion";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import Notification from "./Notification";
+
+const schema = yup.object({
+  name: yup.string().min(3).max(300).required().label("Name"),
+  email: yup.string().max(256).email().required().label("Email"),
+}).required();
 
 const RouteCustomerUpdate = () => {
   const apiURL = useContext(AppContext);
@@ -15,8 +24,14 @@ const RouteCustomerUpdate = () => {
     bgColor: ""
   });
   const { cId } = useParams();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
 
   const token = localStorage.getItem("token");
   const { uId } = jwt(token);
@@ -29,8 +44,8 @@ const RouteCustomerUpdate = () => {
 
       const data = await res.json();
       if (data.message === "success") {
-        setName(data.customer.name);
-        setEmail(data.customer.email);
+        setValue("name", data.customer.name);
+        setValue("email", data.customer.email);
       } else {
         alert(data.message);
       }
@@ -39,18 +54,12 @@ const RouteCustomerUpdate = () => {
     getCustomerInfo();
   }, [cId, token, apiURL]);
 
-  const submit = async (e) => {
-    e.preventDefault();
-
-    if (!name || !email) {
-      return alert("Please enter name and email.");
-    }
-
+  const onSubmit = async (formData) => {
     try {
       const res = await fetch(`${apiURL}/api/customers/${cId}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json", "x-auth-token": token },
-        body: JSON.stringify({ userId: uId, name, email })
+        body: JSON.stringify({ userId: uId, ...formData })
       });
 
       const data = await res.json();
@@ -88,18 +97,38 @@ const RouteCustomerUpdate = () => {
       transition={{ ease: "easeOut", duration: 1.5 }}
     >
       <div className="cont">
-        <form onSubmit={submit}>
-          <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-input-container">
             <label>
               Name
-              <input id="name" type="text" value={name} onChange={(e) => { setName(e.target.value); }} required />
+              <br />
+              <input
+                id="name"
+                type="text"
+                {...register("name")}
+              />
             </label>
+            {errors.name?.message && (
+              <div className='cont-invalid'>
+                <span className='invalid-text'>{errors.name?.message}</span>
+              </div>
+            )}
           </div>
-          <div>
+          <div className="form-input-container">
             <label>
               Email
-              <input id="email" type="email" value={email} onChange={(e) => { setEmail(e.target.value); }} required />
+              <br />
+              <input
+                id="email"
+                type="text"
+                {...register("email")}
+              />
             </label>
+            {errors.email?.message && (
+              <div className='cont-invalid'>
+                <span className='invalid-text'>{errors.email?.message}</span>
+              </div>
+            )}
           </div>
           <div>
             <button className="btn btn-add" id="btn-update" type="submit">Update</button>
